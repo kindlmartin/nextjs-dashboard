14e184e33a62f59e008442d5839c312cc90df2f0
kindldesign <info@kindl.design>
2024-04-26 12:02:09 +0200

    routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-      <Analytics />
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
