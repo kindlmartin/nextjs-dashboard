397fd4b77fe98e842d95fbb76184795dee0f4e8b
Martin Kindl <kindl.m@icloud.com>
2024-12-11 22:12:08 +0100

    form

--- app/ui/customers/actions.tsx
+++ app/ui/customers/actions.tsx
@@ -1,18 +1,17 @@
+'use server';
+
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
-
 const CustomerFormSchema = z.object({
   id: z.string(),
   name: z.string({
     required_error: 'Please fill customer name.',
-    invalid_type_error: 'Please fill customer name.',
+    invalid_type_error: 'Please fill customer name.'
   }),
-  email: z.coerce
-    .number()
-    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  email: z.string().email({ message: 'Please fill valid email.' })
 });
 
 const CreateCustomer = CustomerFormSchema.omit({ id: true });
@@ -27,7 +26,6 @@ export type State = {
 
 export async function createCustomer(prevState: State, formData: FormData) {
   const rawFormData = Object.fromEntries(formData.entries());
-
   const validatedFields = CreateCustomer.safeParse(rawFormData);
 
   if (!validatedFields.success) {
@@ -37,13 +35,14 @@ export async function createCustomer(prevState: State, formData: FormData) {
     };
   }
 
-  const { name, email } = validatedFields.data;
-
   try {
+    const { name, email } = validatedFields.data;
+
     await sql`
-    INSERT INTO customers (name, email)
-    VALUES (${name}, ${email})
-  `;
+        INSERT INTO customers (name, email, image_url)
+        VALUES (${name}, ${email}, '/customers/balazs-orban.png')
+    `;
+
   } catch (e) {
     return {
       message: 'Database Error: Failed to Create Customer.',
--- app/ui/customers/create-form.tsx
+++ app/ui/customers/create-form.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-
 import { useActionState } from 'react';
 import { Button } from '@nextui-org/button';
 import { Input } from '@nextui-org/input';
@@ -10,32 +9,25 @@ import { createCustomer } from '@/app/ui/customers/actions';
 
 export default function CreateCustomerForm() {
   const initialState = { message: '', errors: {} };
-  const [state, dispatch] = useActionState(createCustomer, initialState);
+  const [{ errors }, formAction] = useActionState(createCustomer, initialState);
 
   return (
     <div className="rounded-md bg-gray-50 p-4 md:p-6">
-      <Form onSubmit={(e) => {
-        e.preventDefault();
-        let data = new FormData(e.currentTarget);
-
-        dispatch(data);
-      }}>
-        <Input label="Name" type="string" key="name" variant="bordered" size="sm" isRequired
-               isInvalid={!!state.errors?.name}
-               errorMessage={state.errors?.name?.[0]} />
-        <Input label="Email" type="string" key="email" variant="bordered" size="sm" isRequired
-               isInvalid={!!state.errors?.email}
-               errorMessage={state.errors?.name?.[0]} />
+      <Form action={formAction} validationErrors={errors}>
+        <Input label="Name" name="name" variant="bordered" size="sm" />
+        <Input label="Email" name="email" variant="bordered" size="sm" />
         <div className="mt-6 flex justify-end gap-4">
           <Button
             href="/dashboard/customers"
             as={Link}
-            color="primary"
-            variant="solid"
           >
             Cancel
           </Button>
-          <Button type="submit">Create Customer</Button>
+          <Button
+            type="submit"
+            color="primary"
+            variant="solid"
+          >Create Customer</Button>
         </div>
       </Form>
     </div>
